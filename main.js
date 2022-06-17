dedba5be1b1e1fa63c929d43f047b27922abc96d
denisdelaki <denisdelaki@gmail.com>
2022-06-17 15:39:40 +0300

    Done.

--- main.js
+++ main.js
@@ -2,8 +2,35 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 
+
+// Your JavaScript code goes here!
+//grab the like=glyph reference and add event listener
+const likeHeart = document.querySelectorAll(".like-glyph");
+for (const like of likeHeart) {
+  like.addEventListener("click", handleLikes);
+}
+//create a function to handle likes 
+function handleLikes(e) {
+  const likedHeart = e.target;
+  mimicServerCall("Url")
+    .then(() => {
+      if ( likedHeart.innerText === EMPTY_HEART) {
+        likedHeart.innerText = FULL_HEART;
+        likedHeart.className = "activated-heart";
+      } else {
+        likedHeart.innerText = EMPTY_HEART;
+        likedHeart.className = "";
+      }
+    })
+    //create an error rection 
+    .catch(error => {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
 
 
 
